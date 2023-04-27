18e5fd25582e41261ed1d94f4c15244fb69bb4e9
inaa123 <dlsdk7912@naver.com>
2023-04-28 00:36:25 +0900

    '0428'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import EventPractice from "./EventPractice";
 
-const App = () => {
-  return <EventPractice />;
-};
+import { Component } from "react";
+import ScrollBox from "./ScrollBox";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref)=>{this.scrollBox=ref}}/>
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
